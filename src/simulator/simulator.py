f6b1c3e98dd2f322f4a16751ec8a9fe428dbd79e
devnatanaelsantos <devnatanaelsantos@outlook.com>
2024-06-25 17:12:11 -0300

    refactor: rename variables and keys

--- src/simulator/simulator.py
+++ src/simulator/simulator.py
@@ -20,82 +20,74 @@ class Simulator:
         
         raise Exception("Invalid body")
     
-    def __calc_req_value(self, g_price: float, a_price: float, validated_data: Dict) -> Dict:
+    def __calc_req_value(self, gas_price: float, alc_price: float, validated_data: Dict) -> Dict:
         #Simula a distância percorrida com base na velocidade média de circulação dos veículos em sp (22,4 km/h) e na quantidade de horas trabalhadas
         dist_traveled = 22.4 * float(validated_data["hr_trabalhadas"])
 
         if "km/l_g" in validated_data and "km/l_a" in validated_data:
             #Calcula o consumo de conbustíve por km percorrido
-            g_veh_cons = 1 / float(validated_data["km/l_g"])
-            a_veh_cons = 1 / float(validated_data["km/l_a"])
+            gas_veh_cons = 1 / float(validated_data["km/l_g"])
+            alc_veh_cons = 1 / float(validated_data["km/l_a"])
                 
             #Calcula o consumo total de conbustível com base na distância percorrida
-            g_cons = dist_traveled * g_veh_cons
-            a_cons = dist_traveled * a_veh_cons
+            gas_cons = dist_traveled * gas_veh_cons
+            alc_cons = dist_traveled * alc_veh_cons
 
             #Calcula o custo total de conbustível
-            g_cost = g_cons * g_price
-            a_cost = a_cons * a_price
+            gas_cost = gas_cons * gas_price
+            alc_cost = alc_cons * alc_price
 
             #Estima o valor necessario a acumular para obter o valor almejado
-            g_req_value = g_cost + validated_data["val_almejado"]
-            a_req_value = a_cost + validated_data["val_almejado"]
+            gas_req_value = gas_cost + validated_data["val_almejado"]
+            alc_req_value = alc_cost + validated_data["val_almejado"]
             
-            required_value = {"cost_g": g_cost,
-                                "cost_a": a_cost, 
-                                "req_value_g": g_req_value, 
-                                "req_value_a": a_req_value
+            required_value = {"cost_gas": gas_cost,
+                                "cost_alc": alc_cost, 
+                                "req_value_gas": gas_req_value, 
+                                "req_value_alc": alc_req_value
                                 }
 
             return required_value
            
         elif "km/l_g" in validated_data:
-            g_veh_cons = 1 / float(validated_data["km/l_g"])
-
-            g_cons = dist_traveled * g_veh_cons
-
-            g_cost = g_cons * g_price
-
-            g_req_value = g_cost + validated_data["val_almejado"]
-
-            required_value = {"cost_g": g_cost, 
-                                "req_value_g": g_req_value 
+            gas_veh_cons = 1 / float(validated_data["km/l_g"])
+            gas_cons = dist_traveled * gas_veh_cons
+            gas_cost = gas_cons * gas_price
+            gas_req_value = gas_cost + validated_data["val_almejado"]
+            required_value = {"cost_gas": gas_cost, 
+                                "req_value_gas": gas_req_value 
                                 }
             
             return required_value
         
         else:
-            a_veh_cons = 1 / float(validated_data["km/l_a"])
-
-            a_cons = dist_traveled * a_veh_cons
-
-            a_cost = a_cons * a_price
-
-            a_req_value = a_cost + validated_data["val_almejado"]
-
-            required_value = {"cost_a": a_cost, 
-                                "req_value_a": a_req_value
+            alc_veh_cons = 1 / float(validated_data["km/l_a"])
+            alc_cons = dist_traveled * alc_veh_cons
+            alc_cost = alc_cons * alc_price
+            alc_req_value = alc_cost + validated_data["val_almejado"]
+            required_value = {"cost_alc": alc_cost, 
+                                "req_value_alc": alc_req_value
                                 }
         
             return required_value
 
     def __format_response(self, required_value: Dict) -> Dict:
-            if "cost_g" in required_value and "cost_a" in required_value:
+            if "cost_gas" in required_value and "cost_alc" in required_value:
                 return {
-                    "custo_g": round(required_value["cost_g"], 2),
-                    "custo_a": round(required_value["cost_a"], 2),
-                    "val_necessario_g": round(required_value["req_value_g"], 2),
-                    "val_necessario_a": round(required_value["req_value_a"], 2),
+                    "custo_gas": round(required_value["cost_gas"], 2),
+                    "custo_alc": round(required_value["cost_alc"], 2),
+                    "val_necessario_gas": round(required_value["req_value_gas"], 2),
+                    "val_necessario_alc": round(required_value["req_value_alc"], 2),
                     }
             
-            elif "cost_g" in required_value:
+            elif "cost_gas" in required_value:
                 return {
-                    "custo_g": round(required_value["cost_g"], 2),
-                    "val_necessario_g": round(required_value["req_value_g"], 2),
+                    "custo_gas": round(required_value["cost_gas"], 2),
+                    "val_necessario_gas": round(required_value["req_value_gas"], 2),
                     }
             
             else:
                 return {
-                    "custo_a": round(required_value["cost_a"], 2),
-                    "val_necessario_a": round(required_value["req_value_a"], 2),
+                    "custo_alc": round(required_value["cost_alc"], 2),
+                    "val_necessario_alc": round(required_value["req_value_alc"], 2),
                     }
